615f86667f8075675dddf5b2d11cd656dcf11a91
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2025-02-17 13:41:25 +0100

    added tags to attractions

--- src/main/java/com/example/touristguide/model/TouristAttraction.java
+++ src/main/java/com/example/touristguide/model/TouristAttraction.java
@@ -1,13 +1,24 @@
 package com.example.touristguide.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TouristAttraction {
 
     private String name;
     private String description;
+    private List<AttractionTags> tags;
 
     public TouristAttraction(String name, String description){
         this.name = name;
         this.description = description;
+        this.tags = new ArrayList<>();
+    }
+
+    public TouristAttraction(String name, String description, List<AttractionTags> tags){
+        this.name = name;
+        this.description = description;
+        this.tags = tags;
     }
 
     public String getName(){
@@ -24,5 +35,20 @@ public class TouristAttraction {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void addTag(AttractionTags tag){
+        tags.add(tag);
+    }
+
+    public boolean removeTag(AttractionTags tag){
+        for(AttractionTags t: tags){
+            if(t.equals(tag)){
+                tags.remove(tag);
+                return true;
+            }
+        }
+
+        return false;
     }
 }
